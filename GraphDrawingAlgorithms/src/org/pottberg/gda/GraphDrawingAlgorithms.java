ced0a2d37b52faeedc5aaab49a0d6f235e458dc8
Sören Pottberg <soerenpottberg@web.de>
2014-11-10 15:37:27 +0100

    Format and adjust window size

--- GraphDrawingAlgorithms/src/org/pottberg/gda/GraphDrawingAlgorithms.java
+++ GraphDrawingAlgorithms/src/org/pottberg/gda/GraphDrawingAlgorithms.java
@@ -1,27 +1,29 @@
 package org.pottberg.gda;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class GraphDrawingAlgorithms extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("gui\\view\\GraphDrawingAlgorithms.fxml"));
-			Scene scene = new Scene(root,800,800);
-			scene.getStylesheets().add(getClass().getResource("gui\\view\\application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+    @Override
+    public void start(Stage primaryStage) {
+	try {
+	    BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource(
+		"gui\\view\\GraphDrawingAlgorithms.fxml"));
+	    Scene scene = new Scene(root, 900, 900);
+	    scene.getStylesheets()
+		.add(getClass().getResource("gui\\view\\application.css")
+		    .toExternalForm());
+	    primaryStage.setScene(scene);
+	    primaryStage.show();
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    }
+
+    public static void main(String[] args) {
+	launch(args);
+    }
 }
